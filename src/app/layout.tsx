47afa7b379aa25ba06a2aa542acf81fd504d0b0d
Fernando Balbino <dev.fernandobalbino@gmail.com>
2025-08-31 22:03:48 -0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -9,6 +9,12 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  weight: "400",
+  variable: "--font-plus-jakarta-sans",
   subsets: ["latin"],
 });
 
@@ -24,9 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={` ${plusJakartaSans.variable} antialiased`}>
         {children}
       </body>
     </html>
--- src/components/ui/menuMobile.tsx
+++ src/components/ui/menuMobile.tsx
@@ -1,21 +1,48 @@
 import Image from "next/image";
-import { IoRestaurant } from "react-icons/io5";
-
+import { IoRestaurantOutline } from "react-icons/io5";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 import Link from "next/link";
+
 export default function MenuMobile() {
   return (
     <>
-      <div className="fixed flex z-50  bg-amber-300 bottom-0 rounded-t-xl border-t-2 border-[#e5e7eb] items-center   p-4  w-full">
-        <div>
-          <Image src="/logo.svg" alt="Menu" width={50} height={50} />
-        </div>
-        <div className=" flex justify-around w-full p-4">
-          <Link href="/cardapio">
-            <IoRestaurant size={30} />
+      <div className="fixed flex z-50 bg-white bottom-0 rounded-t-xl items-center ring-4 ring-yellow-300 p-4 w-full">
+        {/* botão central (Início) */}
+        <Link
+          href="/"
+          className="absolute flex flex-col items-center justify-center -top-8 left-1/2 transform -translate-x-1/2 cursor-pointer"
+        >
+          <div className="flex flex-col items-center justify-center">
+            <div className="shadow-lg border-2 border-amber-300 w-17 h-17 bg-white rounded-full flex items-center justify-center">
+              <Image src="/logo.svg" alt="Logo" width={70} height={70} />
+            </div>
+            <div className="uppercase font-bold  text-[3vw]">
+              Página principal
+            </div>
+          </div>
+        </Link>
+
+        <div className="flex justify-around w-full relative">
+          {/* Cardápio */}
+          <Link
+            className="text-[3vw] justify-center items-center flex flex-col-reverse"
+            href="/cardapio"
+          >
+            <div className="font-bold">Cardápio</div>
+            <IoRestaurantOutline size={26} />
           </Link>
-          <Link href="/">Home</Link>
-          <Link href="/">Home</Link>
-          <Link href="/">Home</Link>
+
+          {/* Carrinho */}
+          <Link
+            className="text-[3vw] justify-center items-center flex flex-col-reverse"
+            href="/carrinho"
+          >
+            <div className="font-bold">Carrinho</div>
+            <div className="relative flex">
+              <AiOutlineShoppingCart size={26} />
+              <span className="absolute top-0 -right-2 inline-flex size-2 rounded-full bg-red-500"></span>
+            </div>
+          </Link>
         </div>
       </div>
     </>
