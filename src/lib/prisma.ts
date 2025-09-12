0d47a16d87ae1bbfd6fa82b60ac0f213dffb219a
Fernando Balbino <dev.fernandobalbino@gmail.com>
2025-09-12 16:09:59 -0300

    update

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,19 @@
+// src/lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"], // Opcional: para debug
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// eslint-disable-next-line no-undef
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
-
-// Remova qualquer linha com @ts-ignore
