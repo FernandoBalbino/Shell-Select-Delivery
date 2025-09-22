0b1862abca2daa4adbbb8e5b4948122eb4813473
Fernando Balbino <dev.fernandobalbino@gmail.com>
2025-09-22 14:56:14 -0300

    u

---
+++ src/app/(menuMobile)/teste/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <iframe
+        className="border-0 w-full h-full max-w-6xl max-h-screen"
+        src="https://lookerstudio.google.com/embed/reporting/358922ee-7ea7-48e3-9a9a-69f730826669/page/j6dYF"
+        allowFullScreen
+        sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
+      />
+    </div>
+  );
+}
